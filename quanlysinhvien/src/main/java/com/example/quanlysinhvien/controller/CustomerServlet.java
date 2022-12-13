669ba6dff54c25d7701874742de481bcaef109c2
dung1993 <crorstar1704@gmail.com>
2022-12-13 08:21:52 +0700

    baitap

--- quanlysinhvien/src/main/java/com/example/quanlysinhvien/controller/CustomerServlet.java
+++ quanlysinhvien/src/main/java/com/example/quanlysinhvien/controller/CustomerServlet.java
@@ -25,7 +25,7 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        RequestDispatcher requestDispatcher;
+
         String action = req.getParameter("action");
         if (action == null) {
             action = "";
